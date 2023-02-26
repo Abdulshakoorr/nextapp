944313b5159093347260e13ce25fc646cb27d733
Shakoorkhana <as4630425@gmail.com>
2023-02-26 08:50:17 +0500

    layout

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-slate-100 py-6 px-4 sm:px-8 md:px-16 shadow-md'>Header</div>
+  )
+}
+
+export default Header
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,8 +1,11 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+        <Header />
+    </>
   )
 }
 
